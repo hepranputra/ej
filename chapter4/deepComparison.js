c4428267af16976bb6c759a21cae52dabe7e8645
Hepran Putra <hepranputra@proton.me>
2024-06-06 13:59:36 +0700

    refactor
    Deep Comparison exercise in chapter 4.

--- chapter4/deepComparison.js
+++ chapter4/deepComparison.js
@@ -7,36 +7,27 @@ console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
 
 function deepEqual(x, y) {
-    // Test 1
-    if (x ===
-     y) {
+    // Compare without type conversion.
+    if (x === y) {
         return true;
     }
-    
-    // Test 2
+    // Compare data type.
     if (x == null || typeof x != "object" ||
         y == null || typeof y != "object") {
-        
+            
         return false;
     }
-    
-    
     let keysX = Object.keys(x);
     let keysY = Object.keys(y);
-    
-    // Test 3
+    // Compare length of array's element or object's key.
     if (keysX.length != keysY.length) {
         return false;
     }
-    
-    // Test 4
+    // Compare availability.
     for (let key of keysX) {
-        if (!keysY.includes(key) ||
-            !deepEqual(x[key], y[key])) {
-                
+        if (!keysY.includes(key) || !deepEqual(x[key], y[key])) {
             return false;
         }
     }
-    
     return true;
 }
